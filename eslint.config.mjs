d2e0c9017b47654b88d9dae8b7c3bf9720d57f1f
Viet Nguyen T. <n14t4v2000@gmail.com>
2026-02-04 14:38:16 +0700

    chore(tooling): setup eslint, prettier, husky and lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,7 +29,9 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
